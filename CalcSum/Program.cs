2123d8d57a4c5c8cbe25202d3fd905f82eabaeee
Viacheslav Fomenko <vt222_fva@student.ztu.edu.ua>
2024-01-28 16:40:15 +0200

    add CalcSum

---
+++ CalcSum/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{
+    Console.Write("enter first num - ");
+    double num1 = double.Parse(Console.ReadLine());
+    Console.Write("enter second num - ");
+    double num2 = double.Parse(Console.ReadLine());
+    Console.WriteLine("{1}+{2}={0}", num1 + num2, num1, num2);
+}
+catch
+{
+    Console.WriteLine("One of the numers are incorrect");
+}
